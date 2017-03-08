0332899067b3c00445b05e8b0e8c3c74016008e6
Poga Po <poga.bahamut@gmail.com>
2017-03-08 22:47:19 +0800

    toggle style based on step

--- index.js
+++ index.js
@@ -8,13 +8,10 @@ var container = document.getElementById('vis')
 
 // Create a DataSet (allows two way data-binding)
 var items = new vis.DataSet([
-  { id: 1, content: 'item 1', start: '2013-04-20', end: '2013-04-21', owner: 'foo' },
-  { id: 2, content: 'Step 1', start: '2013-04-20' },
-  { id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19' }
+  { id: 1, content: 'item 1', start: moment('2013-04-20'), end: moment('2013-04-21'), owner: 'foo' },
+  { id: 2, itemType: 'step', content: 'Step 1', start: moment('2013-04-20'), style: 'background-color: grey; border: none; color: white' },
+  { id: 4, content: 'item 4', start: moment('2013-04-16'), end: moment('2013-04-19') }
 ])
-items.on('*', (e, props, sender) => {
-  console.log(e, props, sender)
-})
 
 // Configuration for the Timeline
 var options = {
@@ -43,6 +40,10 @@ var options = {
   onMove: (item, cb) => {
     cb(item)
     updated()
+  },
+  snap: (date, scale, step) => {
+    // snap to start of a day
+    return moment(date).hour(0).minute(0).second(0).millisecond(0)
   }
 }
 
@@ -60,33 +61,66 @@ const view = () => {
       <div class="overflow-auto">
         <table class="f6 w-100 mw8 center" cellspacing="0">
           <thead>
-            <tr class="stripe-dark">
-              <th class="fw6 tl pa3 bg-white">日期</th>
-              <th class="fw6 tl pa3 bg-white">工作項目</th>
-              <th class="fw6 tl pa3 bg-white">負責對象</th>
+            <tr class="bg-black-40 white-90">
+              <th class="fw6 tl pa2">日期</th>
+              <th class="fw6 tl pa2">工作項目</th>
+              <th class="fw6 tl pa2">負責對象</th>
             </tr>
           </thead>
-          <tbody class="lh-copy">
-            ${items.get({order: (x, y) => moment(x.start).isAfter(moment(y.start))}).map(itemView)}
-          </tbody>
+          ${tbody(items.get({order: sortItem}))}
         </table>
       </div>
     </div>
   `
+
+  function sortItem (x, y) {
+    var tx = moment(x.start)
+    var ty = moment(y.start)
+    console.log(x.content, tx.format(), y.content, ty.format())
+    // 如果兩個 item 時間一樣，把 step 放前面
+    if (tx.isSame(ty)) {
+      console.log('same')
+      return x.itemType !== 'step'
+    }
+
+    return moment(x.start).isAfter(moment(y.start))
+  }
 }
 
-const itemView = (item) => {
+const tbody = (items) => {
+  const styles = ['bg-white', 'bg-black-10']
+  var currentStyle = 0
+  console.log(items)
+
+  var views = []
+  items.forEach((item, i) => {
+    if (item.itemType === 'step') {
+      currentStyle = (currentStyle + 1) % styles.length
+      return
+    }
+
+    views.push(itemView(item, i, styles[currentStyle]))
+  })
+
   return yo`
-    <tr class="stripe-dark">
-      <td class="pa3">${formatDate(item.start)} - ${formatDate(item.end)}</td>
-      <td class="pa3">${item.content}</td>
-      <td class="pa3">${item.owner}</td>
+    <tbody class="lh-copy">
+      ${views}
+    </tbody>
+  `
+}
+
+const itemView = (item, i, style) => {
+  return yo`
+    <tr class="${style}">
+      <td class="pa2">${formatDate(item.start)} - ${formatDate(item.end)}</td>
+      <td class="pa2">${item.content}</td>
+      <td class="pa2">${item.owner}</td>
     </tr>
   `
 }
 
 function formatDate (date) {
-  return moment(date).format('YYYY-MM-DD')
+  return moment(date).format('YYYY.MM.DD')
 }
 
 function render () {
